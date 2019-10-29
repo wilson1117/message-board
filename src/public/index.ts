2b57bcce8ea8a298ac97cfd447e10a633667bc42
Cronus <seer20582609@gmail.com>
2019-10-29 11:49:40 +0800

    index page build

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,8 +1,8 @@
-declare var io: any;
-declare var window: any;
-declare var document: any;
-window.onload = () => {
-    var token:string = (new Date() + "").match(/\d+:\d+:\d+/)[0].replace(/:/g, "") + Math.random().toString(36).substr(2);
-    var link = io.connect(document.location.origin);
-    console.log(token);
-}
+// declare var io: any;
+// declare var window: any;
+// declare var document: any;
+// window.onload = () => {
+//     var token:string = (new Date() + "").match(/\d+:\d+:\d+/)[0].replace(/:/g, "") + Math.random().toString(36).substr(2);
+//     var link = io.connect(document.location.origin);
+//     console.log(token);
+// }
